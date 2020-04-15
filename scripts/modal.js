0c51fd8156b9f679819090037ada7d151a2c8d7a
Christopher Peers <christopher_peers@outlook.com>
2020-04-15 17:54:19 +0100

    Adds about box on button click.

---
+++ scripts/modal.js
@@ -0,0 +1,12 @@
+const overlay = document.getElementById('overlay');
+const backgroundImage = document.getElementsByClassName('background-image')[0];
+const backgroundImageOverlay = document.getElementsByClassName('background-image__overlay')[0];
+const contentWrapper = document.getElementsByClassName('content-wrapper')[0];
+const about = document.getElementsByClassName('about')[0];
+
+function openModal(thing) {
+	backgroundImage.classList.toggle('blur');
+	contentWrapper.classList.toggle('content-wrapper--hidden');
+	overlay.style.visibility = 'visible';
+	about.classList.toggle('about--visible');
+}
