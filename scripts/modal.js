83c3543aba0faa976b4b6f9e5c4db79ca4cc1b6a
Christopher Peers <christopher_peers@outlook.com>
2020-04-17 14:53:05 +0100

    Adds modal background animations (forwards only).

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,19 +1,30 @@
 const overlay = document.getElementById('overlay');
 const backgroundImage = document.getElementsByClassName('background-image')[0];
 const backgroundImageOverlay = document.getElementsByClassName('background-image__overlay')[0];
+const backgroundImageContainer = document.getElementsByClassName('background-image-container')[0];
 const contentWrapper = document.getElementsByClassName('content-wrapper')[0];
 const about = document.getElementsByClassName('about')[0];
 
-function openModal(thing) {
-	backgroundImage.classList.toggle('blur');
-	contentWrapper.classList.toggle('content-wrapper--hidden');
-	overlay.style.visibility = 'visible';
-	about.classList.toggle('about--visible');
+function openModal(element) {
+	contentWrapper.classList.add('content-wrapper--hidden');
+	overlay.classList.add('screen-overlay--visible');
+	about.classList.add('about--visible');
+	backgroundImage.classList.add('zoom');
+
+	setTimeout(() => {
+		backgroundImage.style.filter = 'blur(6px)';
+	}, 350)
+
 }
 
 function closeModal() {
-	
-	overlay.style.visibility = 'hidden';
-	about.classList.toggle('about--visible');
-	contentWrapper.classList.toggle('content-wrapper--hidden');
+	contentWrapper.classList.remove('content-wrapper--hidden');
+	overlay.classList.remove('screen-overlay--visible');
+	about.classList.remove('about--visible');
+	backgroundImage.remove('zoom');
+	backgroundImage.classList.add('zoom-out');
+
+	setTimeout(() => {
+		backgroundImage.style.filter = 'none';
+	}, 350)
 }
